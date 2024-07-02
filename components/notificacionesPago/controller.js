6a3f9f03b6a110becdd4e24404cced2dd0a1106c
Sergio Ramos <sergioramosdavid@hotmail.com>
2024-07-02 09:03:39 -0400

    comprobando endpoint

--- components/notificacionesPago/controller.js
+++ components/notificacionesPago/controller.js
@@ -2,6 +2,7 @@ const { addNotificacionDB } = require("./store");
 
 function addNotificacion(transaccion) {
   console.log("----", transaccion);
+  return Promise.resolve();
 }
 module.exports = {
   addNotificacion,
